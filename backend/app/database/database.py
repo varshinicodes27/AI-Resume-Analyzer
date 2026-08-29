3f1f68e15897446e2c784b143e97882c266609d3
varshini <varshinipasapula27@gmail.com>
2026-08-29 15:47:56 +0530

    Fix PyMySQL SSL connection

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,27 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
-import os
-
-# Load environment variables
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Create database engine
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"ssl": {}}
+)
 
-# Create session
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# Base class for models
 Base = declarative_base()
 
-# Dependency
+
 def get_db():
     db = SessionLocal()
     try:
