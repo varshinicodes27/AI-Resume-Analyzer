7fe012b744f06532808fe930b22e5d07d8f2cf6d
varshini <varshinipasapula27@gmail.com>
2026-08-29 18:27:19 +0530

    Fix CORS for React frontend

--- backend/main.py
+++ backend/main.py
@@ -2,39 +2,32 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.auth import router as auth_router
+from app.routers.resume import router as resume_router
 from app.database.database import Base, engine
 from app.models.user import User
 from app.models.resume import Resume
 from app.models.ats_analysis import ATSAnalysis
-from app.routers.resume import router as resume_router
 
-
-# Create all tables
 Base.metadata.create_all(bind=engine)
-
 
 app = FastAPI(
     title="ResumeIQ API",
     version="1.0.0"
 )
 
-
-# Allow React frontend to communicate with FastAPI
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
-# Routers
 app.include_router(auth_router)
 app.include_router(resume_router)
 
