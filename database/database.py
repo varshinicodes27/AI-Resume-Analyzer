11b867931a7df2b9c0452f39f8621fb1cfa4409a
varshini <varshinipasapula27@gmail.com>
2026-08-29 15:42:16 +0530

    Fix production database connection

--- database/database.py
+++ database/database.py
@@ -6,7 +6,9 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    connect_args={
+        "ssl": {}
+    }
 )
 
 SessionLocal = sessionmaker(
